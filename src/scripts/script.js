b75eddf7f4a3e0c926ae00bda613aa5d18e7c857
szyszak <krzszyszko@gmail.com>
2020-07-07 17:03:04 +0200

    initial commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,71 @@
-console.log('HELLO WORLD');
+import { light, dark } from './theme';
+
+const root = window.document.documentElement;
+const toggle = document.querySelector('#toggle');
+
+const getInitialTheme = () => {
+  // use theme saved in local storage
+  const persistedTheme = window.localStorage.getItem('theme');
+  const hasPersistedPreference = typeof persistedTheme === 'string';
+
+  if (hasPersistedPreference) {
+    return persistedTheme;
+  }
+
+  // check if users browser/system supports this media query
+  const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+  const hasMediaQueryPreference = typeof mediaQueryList.matches === 'boolean';
+
+  // use theme preferred by used
+  if (hasMediaQueryPreference) {
+    return mediaQueryList.matches ? 'dark' : 'light';
+  }
+
+  // use default theme
+  return 'light';
+};
+
+const setTheme = (newTheme) => {
+  // update theme in local storage
+  localStorage.setItem('theme', newTheme);
+
+  // update theme toggle state
+  if (newTheme === 'dark') {
+    toggle.checked = true;
+  } else {
+    toggle.checked = false;
+  }
+
+  // change CSS variables according to theme
+  root.style.setProperty('--background', newTheme === 'dark' ? dark.background : light.background);
+
+  root.style.setProperty(
+    '--background-top',
+    newTheme === 'dark' ? dark.backgroundTop : light.backgroundTop,
+  );
+
+  root.style.setProperty('--card-bg', newTheme === 'dark' ? dark.cardBg : light.cardBg);
+
+  root.style.setProperty(
+    '--text-secondary',
+    newTheme === 'dark' ? dark.textSecondary : light.textSecondary,
+  );
+
+  root.style.setProperty(
+    '--text-primary',
+    newTheme === 'dark' ? dark.textPrimary : light.textPrimary,
+  );
+
+  root.style.setProperty(
+    '--card-bg-hover',
+    newTheme === 'dark' ? dark.cardBgHover : light.cardBgHover,
+  );
+
+  root.style.setProperty('--toggle-bg', newTheme === 'dark' ? dark.toggleBg : light.toggleBg);
+
+  root.style.setProperty('--toggle-dot', newTheme === 'dark' ? dark.toggleDot : light.toggleDot);
+};
+
+setTheme(getInitialTheme());
+
+toggle.addEventListener('change', (ev) => setTheme(ev.target.checked ? 'dark' : 'light'));
---
+++ src/scripts/theme.js
@@ -0,0 +1,23 @@
+const light = {
+  toggleBg: 'hsl(230, 22%, 74%)',
+  toggleDot: 'hsl(0, 0%, 100%)',
+  background: 'hsl(0, 0%, 100%)',
+  backgroundTop: 'hsl(225, 100%, 98%)',
+  cardBg: 'hsl(227, 47%, 96%)',
+  textSecondary: 'hsl(228, 12%, 44%)',
+  textPrimary: 'hsl(230, 17%, 14%)',
+  cardBgHover: 'hsl(232, 33%, 91%)',
+};
+
+const dark = {
+  toggleBg: 'linear-gradient(to right, hsl(210, 78%, 56%), hsl(146, 68%, 55%))',
+  toggleDot: 'hsl(232, 19%, 15%)',
+  background: 'hsl(230, 17%, 14%)',
+  backgroundTop: 'hsl(232, 19%, 15%)',
+  cardBg: 'hsl(229, 27%, 20%)',
+  textSecondary: 'hsl(228, 34%, 66%)',
+  textPrimary: 'hsl(0, 0%, 100%)',
+  cardBgHover: 'hsl(228, 26%, 27%)',
+};
+
+export { light, dark };
